af20aadc53ea0430f626926f50788d2aea792fa5
Honey <71226649+piyushji2000@users.noreply.github.com>
2020-09-28 00:00:28 +0530

    Create TurboSort.py

---
+++ TurboSort.py
@@ -0,0 +1,34 @@
+'''
+Given the list of numbers, you are to sort them in non decreasing order.
+Input
+t – the number of numbers in list, then t lines follow [t <= 10^6].
+Each line contains one integer: N [0 <= N <= 10^6]
+
+Output
+Output given numbers in non decreasing order.
+Example
+Input:
+
+5
+5
+3
+6
+7
+1
+Output:
+
+1
+3
+5
+6
+7
+'''
+
+t=int(input())
+a=[]
+for i in range(t):
+	i=int(input())
+	a.append(i)
+a.sort()
+for p in a:
+    print(p)
